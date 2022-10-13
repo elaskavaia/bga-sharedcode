f10d2c00542e75444dac4994aeb3f89bf44d981c
VictoriaLa <laskava@gmail.com>
2022-10-13 09:09:23 -0400

    added argument to read states from
    change yellow to orange and diamond for game states

--- misc/generate_state_diagram.php
+++ misc/generate_state_diagram.php
@@ -3,20 +3,24 @@
 function clienttranslate($x) {
     return $x;
 }
+$infile = isset($argv [1])?$argv [1]:"states.inc.php";
+$extra = isset($argv [2])?$argv [2]:""; // i.e. rankdir=LR;
 
- include("states.inc.php");
+ include($infile);
  echo "digraph D {\n";
+ if ($extra) echo "$extra;\n";
  foreach ($machinestates as $state_id => $state) {
     $color = "red";
     $shape = "ellipse";
     if ($state["name"] == "gameSetup") {
-        $shape = "Mdiamond";
+        $shape = "Msquare";
     }
     if ($state["name"] == "gameEnd") {
         $shape = "Msquare";
     }
     if ($state["type"] == "game") {
-        $color = "yellow";
+        $color = "orange";
+        $shape = "diamond";
     }
     if ($state["type"] == "activeplayer") {
         $color = "blue";
