4aa3e6680abd8036d26b87e86af44c443b7c67f3
VictoriaLa <laskava@gmail.com>
2023-02-13 19:48:40 -0500

    updated genmat with more comments and added directives

--- misc/genmat.php
+++ misc/genmat.php
@@ -8,67 +8,70 @@
  * 
  * Sample input file
  * 
-id|type|name|tooltip|tooltip_action
-train|token|Locomotive|The number of a locomotive indicates the number of spaces it can reach on a railroad:
-factory|token|Factory|
-slot_action_1|slot_action {'o'=>"1,0,1,bb"}|2 Black Track Advancements|This action gives you two advancements of black track. You cannot use this action if you cannot complete all advancements.
+ id|type|name|tooltip|tooltip_action|php
+ train|token|Locomotive|The number of a locomotive indicates the number of spaces it can reach on a railroad:
+ factory|token|Factory|
+ slot_action_1|slot_action|2 Black Track Advancements|This action gives you two advancements of black track. You cannot use this action if you cannot complete all advancements.|'o'=>"1,0,1,bb"
  *
  * Sample material.inc.php BEFORE change
- * If file is not called material.csv, ut foo.csv use  // --- gen php begin foo ---
- * This way can generate multiple sections
- 
 
-$this->token_types = array(
-        // --- gen php begin ---
-         
-        // --- gen php end --- 
-);
+ $this->token_types = array(
+ // --- gen php begin ---
+
+ // --- gen php end --- 
+ );
 
  * After running script
 
-$this->token_types = array(
-        // --- gen php begin ---
-'train' => array(
-  'type' => 'token',
-  'name' => clienttranslate("Locomotive"),
-  'tooltip' => clienttranslate("The number of a locomotive indicates the number of spaces it can reach on a railroad:"),
-),
-'factory' => array(
-  'type' => 'token',
-  'name' => clienttranslate("Factory"),
-),
-'slot_action_1' => array(
-  'type' => 'slot_action',
-  'name' => clienttranslate("2 Black Track Advancements"),
-  'tooltip' => clienttranslate("This action gives you two advancements of black track. You cannot use this action if you cannot complete all advancements."),
-  'o'=>"1,0,1,bb",
-),       
-        // --- gen php end --- 
-);
+ $this->token_types = array(
+ // --- gen php begin ---
+ 'train' => array(
+ 'type' => 'token',
+ 'name' => clienttranslate("Locomotive"),
+ 'tooltip' => clienttranslate("The number of a locomotive indicates the number of spaces it can reach on a railroad:"),
+ ),
+ 'factory' => array(
+ 'type' => 'token',
+ 'name' => clienttranslate("Factory"),
+ ),
+ 'slot_action_1' => array(
+ 'type' => 'slot_action',
+ 'name' => clienttranslate("2 Black Track Advancements"),
+ 'tooltip' => clienttranslate("This action gives you two advancements of black track. You cannot use this action if you cannot complete all advancements."),
+ 'o'=>"1,0,1,bb",
+ ),       
+ // --- gen php end --- 
+ );
 
+ * If file is not called material.csv, if called foo.csv use  // --- gen php begin foo ---
+ * This way can generate multiple sections
+
+Special columns: 
+id - the id of object (string or number)
+variant - special field appended to string as as @xxx  - used to have versions of material file depending on game variants
+-con - way to use php constant to alias an numeric id, defines are currently not injected but dumped on console
+name, tooltip, tooltip_action - default fields which are translatable
+
+#set tr=color - using this directive make 'color' translatable field also
+#set sep=, - change separator
+#set sub=/ - use this character or string to replace default separator, i.e. a/b will be replaced to a|b
+#set noquotes=field - do not quotes for field when outputing
  */
-
 $g_field_names = null;
-
 $g_index = 1;
-
 $g_trans = [ 'name','tooltip','tooltip_action' ];
-
-$g_quotes_id = true;
+$g_separator = '|';
+$g_separator_sub = '';
+$g_noquotes = [];
 
 function handle_header($fields) {
     global $g_field_names;
-    if ($fields [0] == 'idconst') {
-        $fields [0] = 'id';
-        $g_quotes_id = false;
-    }
     if ($fields [0] == 'element_id') {
         // old style header
         $g_field_names = [ 'id','type','name','tooltip','tooltip_action' ];
     } else {
         $g_field_names = $fields;
-    
-        if (array_search('id',$g_field_names)===false) {
+        if (array_search('id', $g_field_names) === false) {
             echo "Error: missing required id column in input file\n";
             exit(2);
         }
@@ -76,13 +79,13 @@ function handle_header($fields) {
     return true;
 }
 
-function get(&$var, $default=null) {
+function get(&$var, $default = null) {
     return isset($var) ? $var : $default;
 }
 
 function isTranslatable($key) {
     global $g_trans;
-    if (array_search($key, $g_trans)!==false) {
+    if (array_search($key, $g_trans) !== false) {
         return true;
     }
     return false;
@@ -92,64 +95,77 @@ function genbody($incsv) {
     global $out;
     $ins = fopen($incsv, "r") or die("Unable to open file! $ins");
     $comment = "";
-    global $g_field_names, $g_index, $g_quotes_id;
-    
-
+    global $g_field_names, $g_index,  $g_separator, $g_separator_sub, $g_trans, $g_noquotes;
     $limit = false;
+    $matches = [ ];
     while ( ($line = fgets($ins)) !== false ) {
-        $line=trim($line);
+        $line = trim($line);
         if (empty($line))
             continue;
-            //special comment
-            #$matches = [ ];
-            #if (preg_match("#default  *(?P<name>\w+)=(?P<value>.*)", $line, $matches)) {
-            #    $g_field_defaults [$matches ['name']] = $matches ['value'];
-            #    continue;
-            #}
         if (startsWith($line, '#')) {
+            //special comment
+            if (preg_match("/#set  *(?P<name>\w+)=(?P<value>.*)/", $line, $matches)) {
+                $key = $matches ['name'];
+                $value = trim($matches ['value']);
+                switch ($key) {
+                    case 'sep':
+                        $g_separator = $value;
+                        break;
+                    case 'sub':
+                        $g_separator_sub = $value;
+                        break;
+                    case 'tr':
+                        $g_trans [] = $value;
+                        break;
+                    case 'noquotes':
+                        $g_noquotes [] = $value;
+                        break;
+                    default:
+                        echo "Error: unknown key $key for #set directive\n";
+                        exit(2);
+                }
+                continue;
+            }
             $comment .= "// $line\n";
             continue;
         }
         if ($g_field_names == null) {
-            $raw_fields = explode('|', $line);
+            $raw_fields = explode($g_separator, $line);
             $limit = count($raw_fields);
             if (handle_header($raw_fields))
                 continue;
         }
-        $raw_fields = explode('|', $line, $limit);
- 
-        $fields=[];
-        $f=0;
-        foreach ($g_field_names as $key) {
-            if (count($raw_fields) >= $f+1)
-                $fields[$key] = $raw_fields [$f];
+        $raw_fields = explode($g_separator, $line, $limit);
+        $fields = [ ];
+        $f = 0;
+        foreach ( $g_field_names as $key ) {
+            if (count($raw_fields) >= $f + 1)
+                $fields [$key] = $raw_fields [$f];
             else
-                $fields[$key]= null;
+                $fields [$key] = null;
             $f++;
         }
-
-        $id=$fields['id'];
+        $id = $fields ['id'];
         $ftype = get($fields ['type'], '');
-        $ftype=varsub($ftype,$fields);
+        $ftype = varsub($ftype, $fields);
         $extra = '';
         // old way of getting extra fields
         $pos = strpos($ftype, '{');
         if ($pos !== false) {
             $extra = substr($ftype, $pos + 1);
             $ftype = trim(substr($ftype, 0, $pos));
-            $fields['type']=$ftype;
+            $fields ['type'] = $ftype;
             $pos = strrpos($extra, '}');
             if ($pos !== false) {
                 $extra = substr($extra, 0, $pos);
-                $fields ['php']=$extra;
+                $fields ['php'] = $extra;
             }
         }
-
         if ($comment) {
             fwrite($out, "$comment");
-            $comment="";
+            $comment = "";
         }
-        $id=varsub($id,$fields);
+        $id = varsub($id, $fields);
         $fullid = $id;
         if (isset($fields ['variant']))
             $fullid = "${id}@" . $fields ['variant'];
@@ -162,57 +178,59 @@ function genbody($incsv) {
                 $concomment = " // $con";
             }
         }
-                   
-        if (is_numeric($fullid)|| $g_quotes_id == false)
+        if (is_numeric($fullid) || array_search('id', $g_noquotes) !== false)
             fwrite($out, " $fullid");
         else
             fwrite($out, " '$fullid'");
-        
         fwrite($out, " => [ ${concomment}\n");
         foreach ( $fields as $key => $value ) {
-            if (startsWith($key, "-")) continue;
-            if ($key=='id') continue;
-            if ($key=='variant') continue;
-            if (!$value && $value!=='0') continue;
-            $value=str_replace("’", "'", $value);
-            $value=str_replace("‘", "'", $value);
-            $value=varsub($value,$fields);
-            if ($key=='php') {
-               // $value=str_replace(",", ",\n  ", $value);// XXX remove      
+            if (startsWith($key, "-"))
+                continue;
+            if ($key == 'id')
+                continue;
+            if ($key == 'variant')
+                continue;
+            if ( !$value && $value !== '0')
+                continue;
+            $value = str_replace("’", "'", $value);
+            $value = str_replace("‘", "'", $value);
+            $value = varsub($value, $fields);
+            if ($g_separator_sub)
+                $value = str_replace($g_separator_sub, $g_separator, $value);
+            if ($key == 'php') {
                 fwrite($out, "  $value,\n");
                 continue;
             }
-         
             if (isTranslatable($key)) {
-                $value=trim($value);
+                $value = trim($value);
                 if ($value)
-                   $exp = "clienttranslate(\"$value\")";
-                else 
-                   continue;
-            } else if (is_numeric($value)) {
+                    $exp = "clienttranslate(\"$value\")";
+                else
+                    continue;
+            } else if (is_numeric($value) ||  array_search($key, $g_noquotes)!==false ) {
                 $exp = $value;
             } else {
                 $exp = "'$value'";
             }
             fwrite($out, "  '$key' => $exp,\n");
-
         }
-        fwrite($out, "],\n");   
-
+        fwrite($out, "],\n");
     }
 }
 
 function startsWith($haystack, $needle) {
     // search backwards starting from haystack length characters from the end
-    return $needle === "" || strrpos($haystack, $needle, - strlen($haystack)) !== false;
+    return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== false;
 }
 
 function endsWith($haystack, $needle) {
     // search backwards starting from haystack length characters from the end
-    return $needle === "" || strrpos($haystack, $needle, - strlen($needle)) !== false;
+    return $needle === "" || strrpos($haystack, $needle, -strlen($needle)) !== false;
 }
 
-/** subsitute variables like {VARIABLE} using keymap*/
+/**
+ * subsitute variables like {VARIABLE} using keymap
+ */
 function varsub($line, $keymap) {
     if ($line === null)
         throw new Exception("varsub: line cannot be null");
@@ -227,7 +245,6 @@ function varsub($line, $keymap) {
     }
     return $line;
 }
-
 // MAIN
 $incsv = $argv [1];
 $basename = basename($incsv, '.csv');
@@ -236,24 +253,18 @@ if ($basename == 'material')
 else
     $module = "$basename ";
 echo "Reading $incsv\n";
-
 if (isset($argv [2])) {
     $infile = $argv [2];
 } else {
     $infile = dirname("$incsv") . "/../material.inc.php";
 }
 echo "Writing $infile\n";
-
-
 $in = fopen($infile, "r") or die("Unable to open file! $in");
 $markup_begin = "--- gen php begin $module---";
 $markup_end = "--- gen php end $module---";
 $outfile = $infile . ".out";
 $out = fopen($outfile, "w") or die("Unable to open file! $outfile");
 //$out = fopen("php://stdout", "w") or die("Unable to open file! $outfile");
-
-
-
 // find markup line
 $found = false;
 while ( ($line = fgets($in)) !== false ) {
@@ -261,11 +272,11 @@ while ( ($line = fgets($in)) !== false ) {
     $line = trim($line);
     $k = strpos($line, $markup_begin);
     if ($k !== false) {
-        $found=true;
+        $found = true;
         break;
     }
 }
-if (!$found) {
+if ( !$found) {
     echo "Error: missing markup $markup_begin in $infile\n";
     unlink($outfile);
     exit(2);
@@ -274,7 +285,7 @@ if (!$found) {
 genbody($incsv);
 // skip all lines until end markup
 while ( ($line = fgets($in)) !== false ) {
-    if (strpos($line, $markup_end ) !== false) {
+    if (strpos($line, $markup_end) !== false) {
         fwrite($out, $line);
         break;
     }
